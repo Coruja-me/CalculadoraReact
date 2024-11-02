0832e9030d655997b4f18826bd1362f9e6bca59d
Coruja <cauavalves@gmail.com>
2024-11-02 18:19:58 -0300

    Preparando o container da calculadora

---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100hv;
+    background-color: #A9A9A9;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Content = styled.div`
+    background-color: #EEE;
+    width: 80%;
+    min-height: 300px;
+`
