5e79c1f5c07e1f1436b1e011685e451b1e6342fe
Coruja <cauavalves@gmail.com>
2024-11-02 19:07:18 -0300

    Criando os botões principais

--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -1,1 +1,11 @@
 import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1.5px solid #888;
+    background-color: #BBB;
+    font-size: 15px;
+    font-weight: 700;
+    height: 70px;
+    flex: 1;
+`
