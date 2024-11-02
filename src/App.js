6d972f5ce441c493b0cdfad5dfa1c9bb69b4fd6b
Coruja <cauavalves@gmail.com>
2024-11-02 19:28:19 -0300

    Desenvolvendo Interações nos botões

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-import { Container, Content, Row, Column } from "./styles";
+import { Container, Content, Row} from "./styles";
 import Input from "./Components/Input";
 import Button from "./Components/Button"
+import { useState } from "react";
 
 
 const App = () => {
+  const [curNum, setCurNum] = useState('0')
+  const handleOnClear = () => {
+    setCurNum('0')
+  }
+  const handleAddNumber = (num) => {
+    setCurNum(prev => `${num}${prev === '0' ? '' : prev}`)
+  }
   return (
     <Container>
       <Content>
-        <Input />
+        <Input value={curNum}/>
         <Row>
-          <Button label={"7"} />
-          <Button label={"8"} />
-          <Button label={"9"} />
-          <Button label={"+"}/>
+          <Button label={"%"} onClick={() => handleAddNumber('')} />
+          <Button label={"/"} onClick={() => handleAddNumber('')} />
+          <Button label={"C"} onClick={handleOnClear} />
+          <Button label={"CE"} onClick={handleOnClear} />
         </Row>
         <Row>
-          <Button label={"4"} />
-          <Button label={"5"} />
-          <Button label={"6"} />
-          <Button label={"-"}/>
+          <Button label={"7"} onClick={() => handleAddNumber('7')} />
+          <Button label={"8"} onClick={() => handleAddNumber('8')} />
+          <Button label={"9"} onClick={() => handleAddNumber('9')} />
+          <Button label={"*"} onClick={() => handleAddNumber('')} />
         </Row>
         <Row>
-          <Button label={"1"} />
-          <Button label={"2"} />
-          <Button label={"3"} />
-          <Button label={"="}/>
+          <Button label={"4"} onClick={() => handleAddNumber('4')} />
+          <Button label={"5"} onClick={() => handleAddNumber('5')} />
+          <Button label={"6"} onClick={() => handleAddNumber('6')} />
+          <Button label={"-"} onClick={() => handleAddNumber('')} />
+        </Row>
+        <Row>
+          <Button label={"1"} onClick={() => handleAddNumber('1')} />
+          <Button label={"2"} onClick={() => handleAddNumber('2')} />
+          <Button label={"3"} onClick={() => handleAddNumber('3')} />
+          <Button label={"+"} onClick={() => handleAddNumber('')} />
+        </Row>
+        <Row>
+          <Button/>
+          <Button label={"0"} onClick={() => handleAddNumber('0')} />
+          <Button label={"."} onClick={() => handleAddNumber('.')} />
+          <Button label={"="} onClick={() => handleAddNumber('=')}/>
         </Row>
       </Content>
     </Container>
--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -8,4 +8,8 @@ export const ButtonContainer = styled.button`
     font-weight: 700;
     height: 70px;
     flex: 1;
+
+    &:hover{
+        opacity: 0.5;
+    }
 `
